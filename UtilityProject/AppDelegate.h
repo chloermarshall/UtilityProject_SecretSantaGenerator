20684534f537a34f517afe028441c6e209935dd9
Chloe Marshall <chloe.r.marshall@gmail.com>
2017-03-28 16:41:04 -0500

    Initial Commit

---
+++ UtilityProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UtilityProject
+//
+//  Created by Chloe Marshall on 3/28/17.
+//  Copyright © 2017 Chloe Marshall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UtilityProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UtilityProject
+//
+//  Created by Chloe Marshall on 3/28/17.
+//  Copyright © 2017 Chloe Marshall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
